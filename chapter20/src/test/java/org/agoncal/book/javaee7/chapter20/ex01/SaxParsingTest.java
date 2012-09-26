8dd0f0a634e242e37c7f002c36938de86d1cb2fa
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-09-26 13:06:04 +0300

    Update chapter20/src/test/java/org/agoncal/book/javaee7/chapter20/ex01/SaxParsingTest.java

--- chapter20/src/test/java/org/agoncal/book/javaee7/chapter20/ex01/SaxParsingTest.java
+++ chapter20/src/test/java/org/agoncal/book/javaee7/chapter20/ex01/SaxParsingTest.java
@@ -13,47 +13,17 @@ import static org.junit.Assert.assertEquals;
  */
 public class SaxParsingTest {
 
-    public static final String ORDER_OUTPUT = "order{\n" +
-            "\tid=1234\n" +
-            "\tdate=05/06/2013\n" +
-            "\tcustomer{\n" +
-            "\t\tfirst_name=James\n" +
-            "\t\tlast_name=Rorrison\n" +
-            "\t\temail{\n" +
-            "\t\t}\n" +
-            "\t\tphoneNumber{\n" +
-            "\t\t}\n" +
-            "\t}\n" +
-            "\tcontent{\n" +
-            "\t\torder_line{\n" +
-            "\t\t\titem=H2G2\n" +
-            "\t\t\tquantity=1\n" +
-            "\t\t\tunit_price{\n" +
-            "\t\t\t}\n" +
-            "\t\t}\n" +
-            "\t\torder_line{\n" +
-            "\t\t\titem=Harry Potter\n" +
-            "\t\t\tquantity=2\n" +
-            "\t\t\tunit_price{\n" +
-            "\t\t\t}\n" +
-            "\t\t}\n" +
-            "\t}\n" +
-            "\tcredit_card{\n" +
-            "\t\tnumber=123412341234\n" +
-            "\t\texpiry_date=10/13\n" +
-            "\t\tcontrol_number=234\n" +
-            "\t\ttype=Visa\n" +
-            "\t}\n" +
-            "}\n";
-
     @Test
-    public void shouldParseOutput() throws Exception {
-
-        SaxParsing saxParsing = new SaxParsing();
-        saxParsing.parseOrderXML();
-        String orderOutput = saxParsing.getOutput();
-
-        assertEquals(ORDER_OUTPUT, orderOutput);
+    public void shouldParseOrderLines() throws Exception {
+        List<OrderLine> parseOrderLines = new SaxParsing().parseOrderLines();
+        assertEquals(2, parseOrderLines.size());
+        OrderLine orderLine = parseOrderLines.get(0);
+        assertEquals("H2G2", orderLine.getItem());
+        assertEquals((Integer) 1, orderLine.getQuantity());
+        assertEquals((Double) 23.5, orderLine.getUnitPrice());
+        orderLine = parseOrderLines.get(1);
+        assertEquals("Harry Potter", orderLine.getItem());
+        assertEquals((Integer) 2, orderLine.getQuantity());
+        assertEquals((Double) 34.99, orderLine.getUnitPrice());
     }
-
 }
