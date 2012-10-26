a6477cebdcb173c9ba967a17528ed1e16ad536b8
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-10-26 18:23:09 +0200

    adding missed files

---
+++ chapter21/chapter21-consumer/src/main/java/org/agoncal/book/javaee7/chapter21/ex10/Main01.java
@@ -0,0 +1,28 @@
+package org.agoncal.book.javaee7.chapter21.ex10;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Main01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Invoking web service programmatically");
+
+        CreditCard01 creditCard = new CreditCard01();
+        creditCard.setNumber("12341234");
+        creditCard.setExpiryDate("10/10");
+        creditCard.setType("VISA");
+        creditCard.setControlNumber(1234);
+
+        CardValidator01 cardValidator = new CardValidator01Service().getCardValidator01Port();
+        System.out.println(cardValidator.validate(creditCard));
+
+        creditCard.setNumber("12341233");
+        System.out.println(cardValidator.validate(creditCard));
+    }
+}
