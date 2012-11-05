78d6e5fe675bf9ab75c43f2750c9a0ab3072a764
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-11-05 17:59:09 +0100

    Adding chapter 14 (Servlet)

--- chapter14/src/main/java/org/agoncal/book/javaee7/chapter14/ex01/ItemServlet01.java
+++ chapter14/src/main/java/org/agoncal/book/javaee7/chapter14/ex01/ItemServlet01.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author Antonio Goncalves
@@ -14,11 +15,12 @@ import java.io.IOException;
  *         http://www.antoniogoncalves.org
  *         --
  */
-@WebServlet(urlPatterns = "itemServlet01")
+@WebServlet(urlPatterns = "/itemServlet01")
 public class ItemServlet01 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("Hello itemServlet01");
     }
 }
