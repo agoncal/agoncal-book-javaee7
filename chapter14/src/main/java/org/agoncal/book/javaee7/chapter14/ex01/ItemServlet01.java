6c5e9cc0ea979a5d34b93309a1c2624d6d3329eb
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-11-05 17:39:23 +0100

    Adding chapter 14 (Servlet)

---
+++ chapter14/src/main/java/org/agoncal/book/javaee7/chapter14/ex01/ItemServlet01.java
@@ -0,0 +1,24 @@
+package org.agoncal.book.javaee7.chapter14.ex01;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@WebServlet(urlPatterns = "itemServlet01")
+public class ItemServlet01 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
