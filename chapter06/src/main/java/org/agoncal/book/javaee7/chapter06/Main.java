da02d5a0c0aa860d3129315138c2b23893827194
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-11-05 14:55:10 +0100

    Adding chapter 6 (JPA)

---
+++ chapter06/src/main/java/org/agoncal/book/javaee7/chapter06/Main.java
@@ -0,0 +1,41 @@
+package org.agoncal.book.javaee7.chapter06;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 6 with Glassfish 3
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creates an instance of book
+        Book book = new Book();
+        book.setTitle("The Hitchhiker's Guide to the Galaxy");
+        book.setPrice(12.5F);
+        book.setDescription("Science fiction comedy series created by Douglas Adams.");
+        book.setIsbn("1-84023-742-2");
+        book.setNbOfPage(354);
+        book.setIllustrations(false);
+
+        // Gets an entity manager and a transaction
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("chapter06PU");
+        EntityManager em = emf.createEntityManager();
+
+        // Persists the book to the database
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(book);
+        tx.commit();
+
+        em.close();
+        emf.close();
+    }
+}
