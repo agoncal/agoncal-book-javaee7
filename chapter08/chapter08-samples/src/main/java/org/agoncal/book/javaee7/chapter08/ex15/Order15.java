70026b250bf4077e1ef9075d7d7d01ab381123e4
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-12-10 23:30:20 +0100

    Adding more EJB code

---
+++ chapter08/chapter08-samples/src/main/java/org/agoncal/book/javaee7/chapter08/ex15/Order15.java
@@ -0,0 +1,11 @@
+package org.agoncal.book.javaee7.chapter08.ex15;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 6 with Glassfish 3
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Order15 {
+}
