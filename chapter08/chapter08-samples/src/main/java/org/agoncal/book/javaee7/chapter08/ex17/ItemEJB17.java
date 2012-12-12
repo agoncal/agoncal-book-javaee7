f836dd4087a905f40752b2ab8a4a39656060b8ef
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-12-12 16:34:54 +0100

    Adding more EJB code & test

---
+++ chapter08/chapter08-samples/src/main/java/org/agoncal/book/javaee7/chapter08/ex17/ItemEJB17.java
@@ -0,0 +1,45 @@
+package org.agoncal.book.javaee7.chapter08.ex17;
+
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+import static org.agoncal.book.javaee7.chapter08.ex17.Book17.FIND_ALL;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Stateless
+@LocalBean
+public class ItemEJB17 implements ItemLocal17, ItemRemote17{
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  @PersistenceContext(unitName = "chapter08PU")
+  private EntityManager em;
+
+  // ======================================
+  // =           Public Methods           =
+  // ======================================
+
+  public List<Book17> findBooks() {
+    TypedQuery<Book17> query = em.createNamedQuery(FIND_ALL, Book17.class);
+    return query.getResultList();
+  }
+
+  public Book17 createBook(Book17 book) {
+    em.persist(book);
+    return book;
+  }
+}
