180f1083a747fd06c3816b21a781dc0aecac33de
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-12-16 22:06:39 +0100

    Adding Putting it all together in chapter 9

--- chapter09/chapter09-consumer/src/main/java/org/agoncal/book/javaee7/chapter09/Main.java
+++ chapter09/chapter09-consumer/src/main/java/org/agoncal/book/javaee7/chapter09/Main.java
@@ -1,6 +1,9 @@
 package org.agoncal.book.javaee7.chapter09;
 
-import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.List;
 
 /**
  * @author Antonio Goncalves
@@ -12,26 +15,22 @@ import javax.ejb.EJB;
 public class Main {
 
   // ======================================
-  // =             Attributes             =
-  // ======================================
-
-  @EJB
-  private static BookEJBRemote bookEJB;
-
-  // ======================================
   // =           Public Methods           =
   // ======================================
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws NamingException {
+
+    // Looks up the EJB
+    Context ctx = new InitialContext();
+    BookEJBRemote bookEJB = (BookEJBRemote) ctx.lookup("java:global/chapter09-service-1.0/BookEJB!org.agoncal.book.javaee7.chapter09.BookEJBRemote");
 
     // Creates an instance of book
-    Book book = new Book();
-    book.setTitle("The Hitchhiker's Guide to the Galaxy");
-    book.setPrice(12.5F);
-    book.setDescription("Science fiction comedy series created by Douglas Adams.");
-    book.setIsbn("1-84023-742-2");
-    book.setNbOfPage(354);
-    book.setIllustrations(false);
+    Book book = new Book("The Hitchhiker's Guide to the Galaxy", 12.5F, "Science fiction by Douglas Adams.", "1-24561-799-0", 354, false);
+
+    List<Book> books = bookEJB.findBooks();
+    for (Book aBook : books) {
+      System.out.println("--- " + aBook);
+    }
 
     book = bookEJB.createBook(book);
     System.out.println("### Book created : " + book);
