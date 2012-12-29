3052b47e8dd847004d3e099b83a053fb9e183779
Antonio Goncalves <antonio.goncalves@gmail.com>
2012-12-29 17:20:28 +0100

    Adding BV code + test

---
+++ chapter03/src/test/java/org/agoncal/book/javaee7/chapter03/ex11/Order11Test.java
@@ -0,0 +1,74 @@
+package org.agoncal.book.javaee7.chapter03.ex11;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.text.ParseException;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Order11Test {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  protected static ValidatorFactory vf;
+  protected static Validator validator;
+
+
+  // ======================================
+  // =          Lifecycle Methods         =
+  // ======================================
+
+  @BeforeClass
+  public static void init() throws ParseException {
+    vf = Validation.buildDefaultValidatorFactory();
+    validator = vf.getValidator();
+  }
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldRaiseNoConstraintsViolation() {
+
+    Order11 order = new Order11();
+    order.setOrderId("CA45678");
+
+    Set<ConstraintViolation<Order11>> constraints = validator.validate(order);
+    assertEquals(0, constraints.size());
+  }
+
+  @Test
+  public void shouldRaiseConstraintsViolationCauseRegex() {
+
+    Order11 order = new Order11();
+    order.setOrderId("FA45678");
+
+    Set<ConstraintViolation<Order11>> constraints = validator.validate(order);
+    displayContraintViolations(constraints);
+    assertEquals(1, constraints.size());
+  }
+
+  private void displayContraintViolations(Set<ConstraintViolation<Order11>> constraintViolations) {
+    for (ConstraintViolation constraintViolation : constraintViolations) {
+      System.out.println("### " + constraintViolation.getRootBeanClass().getSimpleName() +
+              "." + constraintViolation.getPropertyPath() + " - Invalid Value = " + constraintViolation.getInvalidValue() + " - Error Msg = " + constraintViolation.getMessage());
+
+    }
+  }
+}
