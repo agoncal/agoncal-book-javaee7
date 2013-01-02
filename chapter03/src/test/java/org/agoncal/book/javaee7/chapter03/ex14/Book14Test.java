fd8554f12b7a7d9b14a69915e66b16f6ae751f42
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-01-02 17:24:54 +0100

    Adding BV code + test

---
+++ chapter03/src/test/java/org/agoncal/book/javaee7/chapter03/ex14/Book14Test.java
@@ -0,0 +1,98 @@
+package org.agoncal.book.javaee7.chapter03.ex14;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class Book14Test {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  protected static ValidatorFactory vf;
+  protected static Validator validator;
+
+  // ======================================
+  // =          Lifecycle Methods         =
+  // ======================================
+
+  @BeforeClass
+  public static void init() {
+    vf = Validation.buildDefaultValidatorFactory();
+    validator = vf.getValidator();
+  }
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldRaiseNoConstraintViolation() {
+
+    Book14 book = new Book14("H2G2", 12.5f, "Best IT Scifi Book", "1234-4566-9876", 247, false);
+
+    Set<ConstraintViolation<Book14>> constraints = validator.validate(book);
+    assertEquals(0, constraints.size());
+  }
+
+  @Test
+  public void shouldRaiseConstraintViolationCausePriceLow() {
+
+    Book14 book = new Book14("H2G2", 0.5f, "Best IT Scifi Book", "1234-4566-9876", 247, false);
+
+    Set<ConstraintViolation<Book14>> constraints = validator.validate(book);
+    displayContraintViolations(constraints);
+    assertEquals(1, constraints.size());
+  }
+
+  @Test
+  public void shouldRaiseConstraintsViolationCauseTitleAndPriceNull() {
+
+    Book14 book = new Book14();
+
+    Set<ConstraintViolation<Book14>> constraints = validator.validate(book);
+    displayContraintViolations(constraints);
+    assertEquals(2, constraints.size());
+  }
+
+  @Test
+  public void shouldRaiseConstraintsViolationCauseValidatingOnlyTitle() {
+
+    Book14 book = new Book14();
+
+    Set<ConstraintViolation<Book14>> constraints = validator.validateProperty(book, "title");
+    displayContraintViolations(constraints);
+    assertEquals(1, constraints.size());
+  }
+
+  @Test
+  public void shouldRaiseConstraintsViolationCheckingTheTitleValue() {
+
+    Set<ConstraintViolation<Book14>> constraints = validator.validateValue(Book14.class, "title", null);
+    displayContraintViolations(constraints);
+    assertEquals(1, constraints.size());
+  }
+
+  private void displayContraintViolations(Set<ConstraintViolation<Book14>> constraintViolations) {
+    for (ConstraintViolation constraintViolation : constraintViolations) {
+      System.out.println("### " + constraintViolation.getRootBeanClass().getSimpleName() +
+              "." + constraintViolation.getPropertyPath() + " - Invalid Value = " + constraintViolation.getInvalidValue() + " - Error Msg = " + constraintViolation.getMessage());
+
+    }
+  }
+}
