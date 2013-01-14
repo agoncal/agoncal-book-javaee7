ba5b25407348c8e65ce07a52d625aea18266dac0
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-01-14 18:13:12 +0100

    Deleting JSPs

---
+++ chapter11/chapter11-samples/src/main/java/org/agoncal/book/javaee7/chapter11/ex06/BookEJB.java
@@ -0,0 +1,39 @@
+package org.agoncal.book.javaee7.chapter11.ex06;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Stateless
+public class BookEJB {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  @PersistenceContext(unitName = "chapter11PU")
+  private EntityManager em;
+
+  // ======================================
+  // =           Public Methods           =
+  // ======================================
+
+  public List<Book> findBooks() {
+    TypedQuery<Book> query = em.createNamedQuery("findAllBooks", Book.class);
+    return query.getResultList();
+  }
+
+  public Book createBook(Book book) {
+    em.persist(book);
+    return book;
+  }
+}
