a3d688e7af424e207f8f4a56f06bffce20e07a9b
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-01-18 23:26:54 +0100

    Adding JSF Composite Component

---
+++ chapter11/chapter11-samples/src/main/java/org/agoncal/book/javaee7/chapter11/ex14/CD.java
@@ -0,0 +1,71 @@
+package org.agoncal.book.javaee7.chapter11.ex14;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class CD extends Item {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  private String musicCompany;
+  private Integer numberOfCDs;
+  private Float totalDuration;
+  private String genre;
+
+  // ======================================
+  // =            Constructors            =
+  // ======================================
+
+  public CD() {
+  }
+
+  public CD(String title, Float price, String description, String musicCompany, Integer numberOfCDs, Float totalDuration, String genre) {
+    super(title, price, description);
+    this.musicCompany = musicCompany;
+    this.numberOfCDs = numberOfCDs;
+    this.totalDuration = totalDuration;
+    this.genre = genre;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getMusicCompany() {
+    return musicCompany;
+  }
+
+  public void setMusicCompany(String musicCompany) {
+    this.musicCompany = musicCompany;
+  }
+
+  public Integer getNumberOfCDs() {
+    return numberOfCDs;
+  }
+
+  public void setNumberOfCDs(Integer numberOfCDs) {
+    this.numberOfCDs = numberOfCDs;
+  }
+
+  public Float getTotalDuration() {
+    return totalDuration;
+  }
+
+  public void setTotalDuration(Float totalDuration) {
+    this.totalDuration = totalDuration;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+}
---
+++ chapter11/chapter11-samples/src/main/java/org/agoncal/book/javaee7/chapter11/ex14/ItemController.java
@@ -0,0 +1,75 @@
+package org.agoncal.book.javaee7.chapter11.ex14;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Named
+@RequestScoped
+public class ItemController {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  private Book book = new Book();
+  private CD cd = new CD();
+  private Item item = new Item();
+  private List<Item> itemList = new ArrayList<>();
+
+  // ======================================
+  // =           Public Methods           =
+  // ======================================
+
+  public String doCreateBook() {
+    return null;
+  }
+
+  public String doCreateCD() {
+    return null;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public Book getBook() {
+    return book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
+  }
+
+  public CD getCd() {
+    return cd;
+  }
+
+  public void setCd(CD cd) {
+    this.cd = cd;
+  }
+
+  public List<Item> getItemList() {
+    return itemList;
+  }
+
+  public void setItemList(List<Item> itemList) {
+    this.itemList = itemList;
+  }
+
+  public Item getItem() {
+    return item;
+  }
+
+  public void setItem(Item item) {
+    this.item = item;
+  }
+}
