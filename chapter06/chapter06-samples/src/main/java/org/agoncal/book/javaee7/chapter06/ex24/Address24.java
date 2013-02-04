32de3bcb87e1f4f2dbb1ed486c1f2a7306bb2f44
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-02-04 21:37:51 +0100

    Adding samples to module 6

---
+++ chapter06/chapter06-samples/src/main/java/org/agoncal/book/javaee7/chapter06/ex24/Address24.java
@@ -0,0 +1,88 @@
+package org.agoncal.book.javaee7.chapter06.ex24;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Antonio Goncalves
+ *         APress Book - Beginning Java EE 7 with Glassfish 4
+ *         http://www.apress.com/
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Entity
+@Table(name = "ex24_address")
+public class Address24 {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String street1;
+    private String city;
+    private String zipcode;
+    private String country;
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+
+    public Address24() {
+    }
+
+    public Address24(String street1, String city, String zipcode, String country) {
+        this.street1 = street1;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.country = country;
+    }
+
+    // ======================================
+    // =          Getters & Setters         =
+    // ======================================
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
