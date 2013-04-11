b2bc3f2a678d46e4d17657d55b89af5b3f123930
youness-teimoury <youness126@gmail.com>
2013-04-11 12:37:33 +0430

    Create JSONBuilder.java

---
+++ chapter12/src/main/java/org/agoncal/book/javaee7/chapter12/ex05/JSONBuilder.java
@@ -0,0 +1,44 @@
+package org.agoncal.book.javaee7.chapter12.ex05;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+/**
+ * @author Antonio Goncalves
+ * APress Book - Beginning Java EE 7 with Glassfish 4
+ * http://www.apress.com/
+ * http://www.antoniogoncalves.org
+ * --
+ */
+
+public class JSONBuilder {
+    public static void main(String[] args) {
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("order", Json.createObjectBuilder()
+                .add("@id", "1234")
+                .add("@date", "05/06/2013")
+                .add("customer", Json.createObjectBuilder()
+                        .add("@first_name", "James")
+                        .add("@last_name", "Rorrison")
+                        .add("email", "j.rorri@me.com")
+                        .add("phoneNumber", "+44 1234 1234"))
+                .add("content", Json.createObjectBuilder()
+                        .add("order_line", Json.createArrayBuilder()
+                                .add(Json.createObjectBuilder()
+                                        .add("@item", "H2G2")
+                                        .add("@quantity", "1")
+                                        .add("unit_price", "23.5"))
+                                .add(Json.createObjectBuilder()
+                                        .add("@item", "Harry Potter")
+                                        .add("@quantity", "2")
+                                        .add("unit_price", "34.99"))))
+                .add("credit_card", Json.createObjectBuilder()
+                        .add("@number", "1357")
+                        .add("@expiry_date", "10/13")
+                        .add("@control_number", "234")
+                        .add("@type", "Visa")));
+        JsonObject value = objectBuilder.build();
+        System.out.println(value);
+    }
+}
