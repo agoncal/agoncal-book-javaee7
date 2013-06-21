86cb614181e3b5c6271c141718dacbf042c91065
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-06-21 17:47:14 +0200

    Refactoring chapter 9 samples

--- chapter09/chapter09-samples/src/main/java/org/agoncal/book/javaee7/chapter09/ex04/ItemEJB04.java
+++ chapter09/chapter09-samples/src/main/java/org/agoncal/book/javaee7/chapter09/ex04/ItemEJB04.java
@@ -4,7 +4,6 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
